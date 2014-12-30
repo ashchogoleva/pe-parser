73cf4259f60c8b6826f94b7bc2f682e38c72c994
Dmitry Kushnikov <dkushnikov@gmail.com>
2014-12-31 00:04:00 +0300

    Confirm db clearing

--- site/views/site/index.php
+++ site/views/site/index.php
@@ -6,5 +6,5 @@
 $this->title = 'My Yii Application';
 
 ?>
-<a href="/site/clear" target="_blank">Clear DB</a><br/>
+<a href="/site/clear" target="_blank" onclick="return confirm('are you sure?');">Clear DB</a><br/>
 <a href="/site/process" target="_blank">Process files</a><br/>
